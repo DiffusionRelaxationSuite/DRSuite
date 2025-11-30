bd164c8f7d73ecf6f234bfd7386daae39adf926f
David Shattuck <david.shattuck@gmail.com>
2025-11-29 19:11:00 -0800

    Updating phantom_1d_workflow.bat to match current .m file

--- phantom_1d_workflow.bat
+++ phantom_1d_workflow.bat
@@ -1,35 +1,43 @@
 @echo off
+ECHO Creating phantom with 1D spectral encoding.
 set "RootDir=%~dp0"
 set "PATH=%RootDir%bin;%PATH%"
 
+echo on
 
-REM create phantom with 1D spectral encoding
-REM Phantom generation
-if not exist "Result" mkdir Result
-
-"%RootDir%bin\create_phantom.exe" acqfile data/acq_phantom1D.txt spectfile data/Phantom1D_spect.txt outfolder Phantom1D
+echo Creating phantom...
+"create_phantom.exe" acqfile data/acq_phantom1D.txt spectfile data/Phantom1D_spect.txt outfolder Phantom1D multislice 0
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM Spectrum Estimation --ladmm
-"%RootDir%bin\estimate_spectra.exe" imgfile Phantom1D/Phantom_data.mat spatmaskfile Phantom1D/Phantom_mask.mat spect_infofile Phantom1D/Phantom_spectrm_info.mat configfile demos/Phantom1D_ladmm.ini outprefix Result/Phantom1D_ladmm_spect.mat
+echo Running beta sweep...
+"plot_beta_sweep.exe" imgfile Phantom1D/Phantom_data.mat betafile data/betafile_phantom1d.txt spect_infofile Phantom1D/Phantom_spectrm_info.mat outprefix Phantom1D/Phantom_data_ladmm_spect configfile demos/Phantom1D_ladmm.ini spatmaskfile Phantom1D/Phantom_mask_beta_calc.mat file_types png
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo Estimating spectra (ladmm)...
+"estimate_spectra.exe" imgfile Phantom1D/Phantom_data.mat spect_infofile Phantom1D/Phantom_spectrm_info.mat outprefix Phantom1D/Phantom1D_data_ladmm_spect.mat configfile demos/Phantom1D_ladmm.ini spatmaskfile Phantom1D/Phantom_mask.mat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo Estimating spectra (admm)...
+"estimate_spectra.exe" imgfile Phantom1D/Phantom_data.mat spect_infofile Phantom1D/Phantom_spectrm_info.mat outprefix Phantom1D/Phantom1D_data_admm_spect.mat configfile demos/Phantom1D_admm.ini spatmaskfile Phantom1D/Phantom_mask.mat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo Estimating spectra (nnls)...
+"estimate_spectra.exe" imgfile Phantom1D/Phantom_data.mat spect_infofile Phantom1D/Phantom_spectrm_info.mat outprefix Phantom1D/Phantom1D_data_nnls_spect.mat configfile demos/Phantom1D_nnls.ini spatmaskfile Phantom1D/Phantom_mask.mat
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-
-
-
+echo Plotting average spectra...
 REM Plot Average spectra
-"%RootDir%bin\plot_avg_spectra.exe" spect_imfile Result/Phantom1D_ladmm_spect.mat spatmaskfile Phantom1D/Phantom_mask.mat outprefix Result/Phantom1D_data_ladmm_avg_spectra file_types pdf
-
-REM Plot spectroscopic image
-REM encoding sample chosen to show background MR data intensity
-set idx=9
-REM "%RootDir%bin\plot_spect_im.exe" Result/Phantom1D_ladmm_spect.mat Phantom1D/Phantom_data.mat %idx% Phantom1D/Phantom_mask.mat Result/Phantom1D_spectroscopic_Im png
-REM if %errorlevel% neq 0 exit /b %errorlevel%
-
-REM Plot component Maps
-"%RootDir%bin\plot_comp_maps.exe" spect_imfile Result/Phantom1D_ladmm_spect.mat spectmaskfile data/Phantom1D_spectrm_mask.mat color data/four_color.mat outprefix Result/Phantom1D_component_maps file_types epsc
+"plot_avg_spectra.exe" spect_imfile Phantom1D/Phantom1D_data_ladmm_spect.mat spatmaskfile Phantom1D/Phantom_mask.mat outprefix Phantom1D/Phantom1D_data_ladmm_avg_spectra linewidth 3 ax_scale log color g cbar 1 ax_lim "[10 200]" file_types png
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo Plotting spectroscopic image...
+REM Plot spectroscopic image
+"plot_spect_im.exe" spect_imfile Phantom1D/Phantom1D_data_ladmm_spect.mat imgfile Phantom1D/Phantom_data.mat spatmaskfile Phantom1D/Phantom_mask.mat outprefix Phantom1D/Phantom1D_data_spectroscopic_Im threshold .2 linewidth 1 enc_idx 8 ax_scale log color g ax_lim "[10 200]" file_types png
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Plotting component maps...
+REM Plot component Maps
+"plot_comp_maps.exe" spect_imfile Phantom1D/Phantom1D_data_ladmm_spect.mat spectmaskfile data/Phantom1D_spectrm_mask.mat color data/four_color.mat outprefix Phantom1D/Phantom1D_component_maps cbar 0 weights "[1 3]"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Finished phantom 1D workflow!
